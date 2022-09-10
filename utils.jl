cefe50cae31483b98c0960bdfa88277004849dbf
alecloudenback <alecloudenback@users.noreply.github.com>
2022-09-09 22:01:19 -0500

    get math to render? (#29)

--- utils.jl
+++ utils.jl
@@ -1,3 +1,29 @@
+using Artifacts: @artifact_str
+
+"""
+    hfun_artifact(params::Vector{String})::String
+Return a (relative) URL to an artifact.
+For example, for JuliaMono, use
+```
+{{artifact JuliaMono juliamono-0.042 webfonts JuliaMono-Regular.woff2}}
+```
+where JuliaMono is the name of the Artifact in Artifacts.toml.
+This method copies the artifact, puts it into `_assets` and returns a relative URL.
+"""
+function hfun_artifact(params::Vector{String})::String
+    name = params[1]
+    dir = @artifact_str(name)
+    from = joinpath(dir, params[2:end]...)
+    @assert isfile(from)
+    location = params[2:end]
+    to = joinpath(@__DIR__, "__site", "assets", name, location...)
+    mkpath(dirname(to))
+    cp(from, to; force=true)
+    parts = ["assets"; name; location]
+    url = string('/', join(parts, '/'))
+    return url
+end
+
 function hfun_bar(vname)
   val = Meta.parse(vname[1])
   return round(sqrt(val), digits=2)
